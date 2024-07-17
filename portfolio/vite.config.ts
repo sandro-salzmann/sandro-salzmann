24059642f7b01504d8b9532e7974e4d569913d6b
Sandro Salzmann <salzmann2k@gmail.com>
2024-07-17 23:42:36 +0200

    fix: fix reloading page showing a 404

--- portfolio/vite.config.ts
+++ portfolio/vite.config.ts
@@ -2,6 +2,8 @@ import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import { remixRoutes } from "remix-routes/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { copyFileSync } from "node:fs";
+import { join } from "node:path";
 
 export default defineConfig({
   plugins: [
@@ -12,6 +14,20 @@ export default defineConfig({
         v3_fetcherPersist: true,
         v3_relativeSplatPath: true,
         v3_throwAbortReason: true,
+      },
+      buildEnd(args) {
+        if (!args.viteConfig.isProduction) return;
+
+        // When deploying to GitHub Pages, if you navigate from / to another
+        // route and refresh the tab, it will show the default GH Pages 404
+        // page. This happens because GH Pages is not configured to send all
+        // traffic to the index.html where we can load our client-side JS.
+        // To fix this, we can create a 404.html file that contains the same
+        // content as index.html. This way, when the user refreshes the page,
+        // GH Pages will serve our 404.html and everything will work as
+        // expected. (See https://github.com/brookslybrand/remix-gh-pages/blob/main/vite.config.ts)
+        const buildPath = args.viteConfig.build.outDir;
+        copyFileSync(join(buildPath, "index.html"), join(buildPath, "404.html"));
       },
     }),
     tsconfigPaths(),
